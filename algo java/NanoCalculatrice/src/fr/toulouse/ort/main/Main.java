87385ae25ea14f5ae756833d2057d826306f7ea9
Goulwen RIBEILL <goulwen3@hotmail.com>
2019-09-08 15:30:04 +0200

    pas au bon endroit

--- algo java/NanoCalculatrice/src/fr/toulouse/ort/main/Main.java
+++
@@ -1,9 +0,0 @@
-package fr.toulouse.ort.main;
-
-import fr.toulouse.ort.controller.Calculator;
-
-public class Main extends Calculator {
-	public static void main(String[] args) {
-		calcul();
-	}
-}
